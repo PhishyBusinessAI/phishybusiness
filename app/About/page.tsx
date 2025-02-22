797e0021f0067e30999f13b3f1f5b07e59895566
JJTF2003 <30579242+JonJonTheFox@users.noreply.github.com>
2025-02-21 23:58:59 -0500

    Trying to add working about page

---
+++ app/About/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-8">
+            <h1 className="text-3xl font-bold">About Page</h1>
+            <p className="text-lg mt-4">Welcome to the About page! This is where you can describe your app or business.</p>
+        </div>
+    );
+}
